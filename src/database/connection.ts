5d8be223def4ffdc3a35f7cb2bc1844be3cbf5ff
mohamad <mohamadmirzaei.ir@gmail.com>
2022-09-13 23:21:47 +0430

    create database function

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 function connect(cb:(err:Error | any , message:string)=> void):void{
-    mongoose.connect(process.env.MONGODB_URI!)
+    mongoose.connect('mongodb://localhost:27017/url-shortner')
     .then(()=>{
         cb(null , 'connect to database');
     })
